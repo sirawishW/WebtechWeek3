09cb120121abfb38fd6ca74ef2198beb7be00fc6
Sirawish Wongsuthipakorn <sirawish.wo@ku.th>
2021-07-13 17:42:45 +0700

    Fix Label

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import NumberList from '../view/NumberList.vue'
+import NumberList from '../views/NumberList.vue'
 
 Vue.use(VueRouter)
 
